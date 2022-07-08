d6f1bf0e4759e013698faf8086b170c952332ddd
Gene Michael Lauria <lauria.gene@gmail.com>
2022-07-08 16:07:59 -0700

    tutorial complete

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Routes, Route } from "react-router-dom"
+import Home from "./components/Home"
+import About from "./components/About"
+import Nav from "./components/Nav"
+
+const App =()=>{
+    return(
+        <>
+        <Nav />
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="*" element={<Home />} />
+        </Routes>
+        </>
+    )
+}
+
+export default App

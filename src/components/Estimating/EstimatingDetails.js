dab61555c541627c287fb414d9c3d7ecc59abf3b
Gene Michael Lauria <lauria.gene@gmail.com>
2022-07-13 06:23:18 -0700

    Added a toggle switch

---
+++ src/components/Estimating/EstimatingDetails.js
@@ -0,0 +1,162 @@
+import React, { useState } from 'react'
+import NonPrintToggle from '../ReprintToggle'
+import { Container, 
+    Row, 
+    Col, 
+    Label,
+    Input, 
+    Form, 
+    FormGroup, 
+    Alert,
+    ButtonGroup,
+    Button,
+    ButtonToggle
+ } from 'reactstrap'
+ 
+
+const EstimatingDetails = () => {
+    
+    const [nonClickText, setNonClickText] = useState("False");
+   
+    return (
+    <Container>
+    <Form>
+        <h5>Job Details</h5>
+        <p>For a new estimate, fill in details and click Get a Quote Number</p>
+        <p>To load an estimate, fill in the Estimate Number and click Load</p>
+        <Row>
+            <Col>
+                <Label htmlFor='estNumber'>Estimate Number</Label>
+                <Input
+                    type='text'
+                    name='estNumber'
+                    id='estNumber'
+                    disabled>
+                </Input>
+            </Col>
+        </Row>
+        <Row>
+            <Col>
+                <Label htmlFor='cust'>
+                    Customer
+                </Label>
+                <Input
+                type='text'
+                name='cust'
+                id='cust'
+                >
+                </Input>
+                <Label htmlFor='desc'>
+                    Job Title
+                </Label>
+                <Input
+                type='text'
+                name='desc'
+                id='desc'
+                >
+                </Input>
+            </Col>
+            <Col>
+                <Label htmlFor='pm'>
+                    Project Manager
+                </Label>
+                <Input
+                type='select'
+                id='pm'
+                name='pm'
+                className='form-control'
+                >
+                    <option></option>
+                    <option>Merle May Lestrange Lauria Lenssen</option>
+                </Input>
+                <Label 
+                htmlFor='rep'>
+                Sales Rep
+                </Label>
+                <Input
+                type='select'
+                id='rep'
+                name='rep'
+                className='form-control'
+                >
+                    <option></option>
+                <option>Berta Caseres</option>
+                </Input>
+                
+            </Col>
+            <Col>
+                <Label htmlFor='type'>
+                    Job Type
+                </Label>
+                <Input
+                type='select'
+                name='type'
+                id='type'
+                className='form-control'>
+                    <option></option>
+                    <option>New Job</option>
+                    <option>ERP</option>
+                    <option>Reprint w/Changes</option>
+                </Input>
+                <Label htmlFor='prev'>
+                    Previous Job
+                </Label>
+                <Input
+                type='text'
+                name='prev'
+                id='prev'
+                className='form-control'>
+
+                </Input>
+            </Col>
+        </Row>
+        <Row>
+            <Col>
+                <Label
+                htmlFor='non_print'>
+                    Non-printing item?
+                </Label>
+                <Button
+                type='checkbox'
+                id='non-print'
+                onClick={() => {
+                    if(nonClickText=="False"){
+                        setNonClickText("True")
+                    } else {
+                        setNonClickText("False")
+                    }
+                }}
+                >
+                <>
+                {nonClickText}
+                </>
+                </Button>
+            </Col>
+        </Row>
+        <Row className='justify-content-end'>
+        <ButtonGroup>
+            <Button 
+                outline
+                color='info'
+                id='loadButton'
+                >Get a Quote Number
+            </Button>
+            <Button
+                outline
+                color='secondary'
+                id='openButton'
+                >
+                    Load
+                </Button>
+            <Button
+            color='danger'
+            id='clearButton'>Clear</Button>
+        </ButtonGroup>
+        </Row>
+    </Form>
+    </Container>
+    
+  )
+}
+
+export default EstimatingDetails
---
+++ src/components/Estimating/SpecComponent.js
@@ -0,0 +1,205 @@
+import React, { useState, useEffect, useRef } from 'react'
+import ToggleSwitch from '../../ToggleSwitch/ToggleSwitch'
+import { Container, 
+    Row, 
+    Col, 
+    Label,
+    Input, 
+    Form, 
+    FormGroup, 
+    Alert,
+    ButtonGroup,
+    Button,
+    ButtonToggle,
+    Accordion,
+    AccordionHeader,
+    Collapse,
+    Card,
+    CardBody,
+    Table
+ } from 'reactstrap'
+
+const SpecComponent = () => {
+  const [pdfProofActive, setPdfProofActive] = useState(false);
+  const [hardProofActive, setHardProofActive] = useState(false);
+  const [colorMatchActive, setColorMatchActive] = useState(false);
+  const [variableDataActive, setVariableDataActive] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+  const pdfToggleIsTrue = () =>{
+    setPdfProofActive(current => !current);
+  };
+  
+  const hardToggleIsTrue = () =>{
+    setHardProofActive(current => !current);
+  };
+
+  const colorMatchToggleIsTrue = () =>{
+    setColorMatchActive(current => !current);
+  };
+
+  const variableDataToggleIsTrue = () =>{
+    setVariableDataActive(current => !current);
+  };
+
+  const initialToggleValue = useRef(false);
+
+  useEffect(() => {
+    if(initialToggleValue.current) {
+      initialToggleValue.current=true
+      return;
+    }
+    
+  }, [initialToggleValue])
+
+  return (
+    <Container>
+      <h5>Job Components</h5>
+      
+      <Button outline color="info" className="btn-block" onClick={toggle}>
+        Prepress
+      </Button>
+      <Collapse isOpen={isOpen}>
+        <Card>
+          <CardBody className="collapse">
+            <Table bordered hover responsive>
+              <thead>
+                <th>PDF Proof</th>
+                <th>Hard Copy Proof</th>
+                <th>PMS or Color Match</th>
+                <th>Variable Data</th>
+                
+              </thead>
+              <tbody>
+                <td>
+                <ToggleSwitch 
+                  id='pdfProof'
+                  checked={pdfProofActive}
+                  onClick={pdfToggleIsTrue}
+                  onChange={pdfToggleIsTrue}
+                />
+                </td>
+                <td>
+                <ToggleSwitch 
+                  id='hardProof'
+                  checked={hardProofActive}
+                  onClick={hardToggleIsTrue}
+                  onChange={hardToggleIsTrue}
+                />
+                </td>
+                <td>
+                  <ToggleSwitch 
+                  id='colorMatch'
+                  checked={colorMatchActive}
+                  onClick={colorMatchToggleIsTrue}
+                  onChange={colorMatchToggleIsTrue}
+                />
+                </td>
+                <td>
+                    <ToggleSwitch
+                    id='variableData'
+                    checked={variableDataActive}
+                    onClick={variableDataToggleIsTrue}
+                    onChange={variableDataToggleIsTrue}
+                    />
+                </td>
+                
+              </tbody>
+            </Table>
+          </CardBody>
+        </Card>
+      </Collapse>
+      <br />
+      <Button outline color="primary" className="btn-block">
+        Parts - Press & Paper
+      </Button>
+      <Collapse>
+        <Card>
+          <CardBody>
+            <Collapse>
+              <Button outline color="primary">
+                Part 1
+              </Button>
+              <Card>
+                <CardBody>
+                  <Table hover bordered responsive>
+                    <thead>
+                      <th>Press</th>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>
+                          <select id="p1press" className="form-control">
+                            <option>Indigo</option>
+                            <option>Versant 80</option>
+                            <option>KM1250</option>
+                          </select>
+                        </td>
+                      </tr>
+                    </tbody>
+                    <tbody>
+                      <tr>
+                        <th>Notes</th>
+                        <th>Dimension A</th>
+                        <th>Dimension B</th>
+                        <th>Pages</th>
+                        <th>Bleeds</th>
+                      </tr>
+                    </tbody>
+                    <tbody>
+                      <tr>
+                        <td>
+                          <input
+                            type="text"
+                            id="p1notes"
+                            className="form-control"
+                          />
+                        </td>
+                        <td>
+                          <input
+                            type="text"
+                            id="p1d1"
+                            className="form-control"
+                          />
+                        </td>
+                        <td>
+                          <input
+                            type="text"
+                            id="p1d2"
+                            className="form-control"
+                          />
+                        </td>
+                        <td>
+                          <input
+                            type="text"
+                            id="p1pp"
+                            className="form-control"
+                          />
+                        </td>
+                        <td>
+                          <select id="p1bleed" className="form-control">
+                            <option>True</option>
+                            <option>False</option>
+                          </select>
+                        </td>
+                      </tr>
+                      <tr>
+                        <th>Paper</th>
+                        <th>Paper Short Dim</th>
+                        <th>Paper Long Dim</th>
+                        <th>Prepress Trimming</th>
+                        <th>Envelopes</th>
+                      </tr>
+                    </tbody>
+                  </Table>
+                </CardBody>
+              </Card>
+            </Collapse>
+          </CardBody>
+        </Card>
+      </Collapse>
+    </Container>
+  );
+};
+
+export default SpecComponent;
